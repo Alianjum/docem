fa88361793360bab8eaf6dd8d4f4d29abc42cd82
Abhishek Kumar Verma <50478316+B4xAbhishek@users.noreply.github.com>
2023-12-30 07:01:19 +0000

    added product locales

--- locales/en.js
+++ locales/en.js
@@ -140,350 +140,40 @@ export default {
     viewAll: 'View All Features',
     viewLess: 'View Less Features',
     products: {
-      title: "Our Products",
-      subTitle: 'These solutions are available to independent brokers as well.',
-      box1: {
-        title: 'Integrated with MLS',
-        titleQc: 'Integrated with Centris',
-        list: {
-          list1: 'Listings automatically loaded from MLS',
-          list1Qc: 'Listings automatically loaded from Centris',
+        title: "Our Products",
+        subTitle: 'These solutions are available to independent brokers as well.',
+        description: "One single platform for a smarter way of doing real estate. DocEM saves you time, money and makes you handle your business like a real pro adhering to the highest standards in the industry.",
+        eform: {
+            title: 'eFORM/eSign/IDV',
+            description: 'Completely paperless, safe without compromising the identity of your clients. An easy way to access your forms, having them pre-populated and sign effortlessly. Verify the identity of your clients securely and remain compliant. Ensuring the security and integrity of real estate transactions by verifying the identity of individuals in a reliable and efficient manner.'
         },
-      },
-      box2: {
-        title: 'Accounting',
-        list: {
-          list1: 'Integrated with Quickbooks online',
-          list2: 'Invoices, payments synced to QBO',
+        showingManagement: {
+            title: 'Showing Management',
+            description: 'Managing your showing without the need for additional tools. See your listings, showings, offers, and transactions from the same dashboard. Centralizing and automating the process of scheduling and managing property showings, enhancing efficiency, communication, and collaboration among stakeholders involved in the property transaction process.'
         },
-      },
-      box3: {
-        title: 'Invoicing',
-        list: {
-          list1: 'Agents monthly invoice',
-          list2: 'Security deposit',
-          list3: 'Transaction invoices/statements',
+        offerManagement: {
+            title: 'Offer Management',
+            description: 'Streamline and automate the process of creating, submitting, tracking, and managing offers. A seamless workflow between you and the other agent to collaborate. Completely eliminates the need for constant uploading, downloading, and emailing. Enhance efficiency, transparency, collaboration, and compliance throughout the offer negotiation and acceptance process.'
         },
-      },
-      box4: {
-        title: 'Online Payments',
-        list: {
-          list1: 'Pay invoices online',
-          list2: 'Pay by cheque/credit card',
-          list3: 'Collect agent fees',
-          list4: 'Pre Authorized payment',
-          list5: 'Integrated with Paysafe',
+        transactionManagement: {
+            title: 'Transaction Management',
+            description: 'Eliminate the duplication of entries. The initial offer between both agents automatically translates to a transaction specific to each brokerage. Offers secure storage, organization, and management of transaction documents, forms, contracts, and related paperwork. Integrates seamlessly with external systems and platforms to facilitate accounting, payment, and more.'
         },
-      },
-      box5: {
-        title: 'Direct Deposit',
-        list: {
-          list1: 'Pay agents online',
-          list2: 'Pay brokerages online',
-          list3: 'Integrated with most banks',
+        association: {
+            title: 'Association',
+            description: 'Standardize the practice across your members, providing them with the tools that allow them to be better agents without compromising client privacy. Enhance security, streamline operations, improve compliance, and deliver a superior client experience, ultimately contributing to greater efficiency, trust, and success in the real estate market.'
         },
-      },
-      box6: {
-        title: 'Ledgers',
-        titleQc: 'Registers',
-        list: {
-          list1: 'Agent ledger',
-          list1Qc: 'Agent register',
-          list2: 'Listing ledger',
-          list2Qc: 'Listing register',
-          list3: 'Deal ledger',
-          list3Qc: 'Transaction register',
-          list4: 'Trust ledger',
-          list4Qc: 'Trust register',
-          list5: 'Disclosure ledger',
-          list5Qc: 'Notice of disclosure register',
+        propertyManager: {
+            title: "Property Manager's Solution",
+            description: 'A safe way to manage the property records of your landlords, share occupancy terms with your tenants, keep track of service requests, and facilitate the collection of monthly rent.'
         },
-      },
-      box7: {
-        title: 'Showings',
-        list: {
-          list1: 'A streamline and easy process',
-          list2: 'Online appointment',
-          list3: 'Agent/owner approval',
-          list3Qc: 'Broker/owner online approval',
-          list4: 'Notification to listing/buying agent',
-          list4Qc: 'Notification to listing/buying broker',
-          list5: 'Incoming/outgoing showing calendar request',
-          list6: 'Buying agent feedback',
-          list6Qc: 'Buying broker feedback',
-          list7: 'New offer notifications',
-        },
-      },
-      box8: {
-        title: 'Electronic Document Storage',
-        list: {
-          list1: 'Approved by OACIQ',
-          list2: 'Unlimited online storage',
-          list3: 'Servers located in Ontario',
-          list4: 'Servers located in Canada',
-        },
-      },
-      box9: {
-        title: 'eForm and eSignature',
-        list: {
-          list1: 'All OREA forms and clauses',
-          list2: 'Custom brokerage forms/clauses',
-          list2Qc: 'Custom agency forms/clauses',
-          list3: 'Custom agent clauses',
-          list3Qc: 'Custom brokers clauses',
-          list4: 'Unlimited eSignature',
-          list5: 'Signed documents automatically uploaded',
-        },
-      },
-      box10: {
-        title: 'Credit Report',
-        list: {
-          list1: 'Integrated with Equifax',
-          list2: 'Pull up to date  client credit report in real time',
-        },
-      },
-      box11: {
-        title: 'Monitoring',
-        list: {
-          list1: 'Listing expiry date',
-          list2: 'Inspection date',
-          list3: 'Financing date',
-          list4: 'Closing date',
-          list4Qc: 'Notarization date',
-          list5: 'Next invoice date',
-          list6: 'Missing mandatory document',
-        },
-      },
-      box12: {
-        title: 'Tax Report',
-        list: {
-          list1: 'T4A summary',
-          list2: 'T4A slip',
-        },
-      },
-      box13: {
-        title: 'Restricted Access',
-        list: {
-          list1: 'Lawyer',
-          list1Qc: 'Lawyer /Notary',
-          list2: 'Clients',
-        },
-      },
-      box14: {
-        title: 'Reporting',
-        list: {
-          list1: 'Performance report',
-          list2: 'Accounting report',
-          list3: 'Trend report',
-        },
-      },
-      box15: {
-        title: 'Rent',
-        list: {
-          list1: 'Tenant monthly invoice',
-          list2: 'Landlord monthly statement',
-        },
-      },
-      box16: {
-        title: 'Workflow',
-        list: {
-          list1: 'Service request',
-          list2: 'Service contract',
-          list3: 'Maintenance',
-        },
-      },
-    },
-    agent: {
-      title: "Agent's Solution",
-      titleBroker: "Independant Broker's Solution",
-      subTitle:
-        'For agents, DocEM’s platform allows for filing and e-signing all necessary forms to complete a real estate transaction from start to finish.',
-      subTitleQc:
-        'For brokers, DocEM’s platform allows for e-signing all necessary forms to complete a real estate transaction from start to finish.',
-      subTitleBroker:
-        "For Independant Broker's, DocEM’s platform allows for filing and e-signing all necessary forms to complete a real estate transaction from start to finish.",
-      subTitleBrokerQc:
-        "For Independent Broker's, DocEM’s platform allows for e-signing all necessary forms to complete a real estate transaction from start to finish.",
-      box1: {
-        title: 'Integrated with MLS',
-        titleQc: 'Integrated with Centris',
-        list: {
-          list1: 'Your listings automatically loaded from MLS',
-          list1Qc: 'Your listings automatically loaded from Centris',
-        },
-      },
-      box2: {
-        title: 'Electronic Document Management',
-        list: {
-          list1: 'Approved by OACIQ',
-          list2: 'Unlimited online storage',
-          list3: 'Server located in Ontario',
-          list4: 'Server located in Canada',
-        },
-      },
-      box3: {
-        title: 'eForm and eSignature',
-        list: {
-          list1: 'All OREA forms and clauses',
-          list2: 'Custom brokerage forms/clauses',
-          list2Qc: 'Custom agency forms/clauses',
-          list3: 'Custom agent clauses',
-          list3Qc: 'Custom brokers clauses',
-          list4: 'Unlimited eSignature',
-          list5: 'Signed documents automatically uploaded',
-        },
-      },
-      box4: {
-        title: 'Credit Report',
-        list: {
-          list1: 'Integrated with Equifax',
-          list2: 'Pull up to date client credit report in real time',
-        },
-      },
-    },
-    manager: {
-      title: `Property Manager's Solution`,
-      subTitle:
-        'A safe way to manage the property records of your landlords, share occupancy terms with your tenants, keep track of service requests and facilitate the collection of monthly rent.',
-      box1: {
-        title: 'Rent',
-        list: {
-          list1: 'Tenant monthly invoice',
-          list2: 'Landlord monthly statement',
-        },
-      },
-      box2: {
-        title: 'Online Payments',
-        list: {
-          list1: 'Collect rent through Pre Authorized payment',
-          list2: 'Manage NSF',
-          list3: 'Pay invoice online with credit card',
-          list4: 'Integrated with Paysafe',
-        },
-      },
-      box3: {
-        title: 'Electronic Document Management',
-        list: {
-          list1: 'A central repository to manage your property documents',
-          list2: 'Unlimited online storage',
-        },
-      },
-      box4: {
-        title: 'eForm and eSignature',
-        list: {
-          list1: 'Access lease online',
-          list1On: 'Access Residential Tenancy Agreement online',
-          list2: 'Access landlord/tenant forms and notices online',
-          list2On: 'Access landlord/tenant forms and notices online',
-          list3: 'Unlimited eSignature',
-          list4: 'Signed documents automatically uploaded',
-        },
-      },
-      box5: {
-        title: 'Credit Report',
-        list: {
-          list1: 'Integrated with Equifax',
-          list2: 'Pull up to date client credit report in real time',
-          list3: 'Integrated with most banks',
-        },
-      },
-      box6: {
-        title: 'Monitoring',
-        list: {
-          list1: 'Rent increase',
-          list2: 'End lease',
-          list3: 'Maintenance',
-          list4: 'Service contract',
-        },
-      },
-      box7: {
-        title: 'Workflow',
-        list: {
-          list1: 'Service request',
-          list2: 'Service contract',
-          list3: 'Maintenance',
-        },
-      },
-      box8: {
-        title: 'Reporting',
-        list: {
-          list1: 'Income',
-          list2: 'Expense',
-        },
-      },
-    },
-    landlord: {
-      title: `Landlord's Solution`,
-      subTitle:
-        'An efficient approach to manage your property and keeping track of the most important task.',
-      box1: {
-        title: 'Rent',
-        list: {
-          list1: 'Tenant monthly invoice',
-          list2: 'Landlord monthly statement',
-        },
-      },
-      box2: {
-        title: 'Online Payments',
-        list: {
-          list1: 'Collect rent through Pre Authorized payment',
-          list2: 'Manage NSF',
-          list3: 'Pay invoice online with credit card',
-          list4: 'Integrated with Paysafe',
-        },
-      },
-      box3: {
-        title: 'Electronic Document Management',
-        list: {
-          list1: 'A central repository to manage your property documents',
-          list2: 'Unlimited online storage',
-        },
-      },
-      box4: {
-        title: 'eForm and eSignature',
-        list: {
-          list1: 'Access lease online',
-          list1On: 'Access Residential Tenancy Agreement online',
-          list2: 'Access landlord/tenant forms and notices online',
-          list2On: 'Access landlord/tenant forms and notices online',
-          list3: 'Unlimited eSignature',
-          list4: 'Signed documents automatically uploaded',
-        },
-      },
-      box5: {
-        title: 'Credit Report',
-        list: {
-          list1: 'Integrated with Equifax',
-          list2: 'Pull up to date client credit report in real time',
-          list3: 'Integrated with most banks',
-        },
-      },
-      box6: {
-        title: 'Monitoring',
-        list: {
-          list1: 'Rent increase',
-          list2: 'End lease',
-          list3: 'Maintenance',
-          list4: 'Service contract',
-        },
-      },
-      box7: {
-        title: 'Workflow',
-        list: {
-          list1: 'Service request',
-          list2: 'Service contract',
-          list3: 'Maintenance',
-        },
-      },
-      box8: {
-        title: 'Reporting',
-        list: {
-          list1: 'Income',
-          list2: 'Expense',
-        },
-      },
-    },
-  },
+        landlord: {
+            title: "Landlord's Solution",
+            description: 'An efficient approach to manage your property and keep track of the most important tasks.'
+        }
+    }
+},
+
 
   features: {
     viewAll: 'View All Features',
